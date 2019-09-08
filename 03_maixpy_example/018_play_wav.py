2ea275183d8baff97c062e5c85e6022c59d755b9
anoken <31365453+anoken@users.noreply.github.com>
2019-09-09 08:37:05 +0900

    Add files via upload

---
+++ 03_maixpy_example/018_play_wav.py
@@ -0,0 +1,45 @@
+## Copyright (c) 2019 aNo研 プリンをもっと見守る技術 
+## https://github.com/anoken/purin_wo_motto_mimamoru_gijutsu
+from fpioa_manager import *
+from Maix import I2S, GPIO
+import audio
+
+##スピーカの初期化
+def init_wav():
+    fm.register(board_info.SPK_SD, fm.fpioa.GPIO0)
+    spk_sd=GPIO(GPIO.GPIO0, GPIO.OUT)
+    spk_sd.value(1)
+    fm.register(board_info.SPK_DIN,fm.fpioa.I2S0_OUT_D1)
+    fm.register(board_info.SPK_BCLK,fm.fpioa.I2S0_SCLK)
+    fm.register(board_info.SPK_LRCLK,fm.fpioa.I2S0_WS)
+    wav_dev = I2S(I2S.DEVICE_0)
+
+##wavファイルの再生
+def play_wav(fname):
+    player = audio.Audio(path = fname)
+    player.volume(20)
+    wav_info = player.play_process(wav_dev)
+    wav_dev.channel_config(wav_dev.CHANNEL_1,
+        I2S.TRANSMITTER,resolution = I2S.RESOLUTION_16_BIT,
+        align_mode = I2S.STANDARD_MODE)
+    wav_dev.set_sample_rate(wav_info[1])
+    while True:
+        ret = player.play()
+        if ret == None:
+            break
+        elif ret==0:
+            break
+    player.finish()
+
+fm.register(board_info.BUTTON_A, fm.fpioa.GPIO1)
+but_a=GPIO(GPIO.GPIO1, GPIO.IN, GPIO.PULL_UP)
+but_a_pressed = 0
+
+while True:
+    if but_a.value() == 0 and but_a_pressed == 0:
+        play_sound("reset.wav")
+        but_a_pressed=1
+    if but_a.value() == 1 and but_a_pressed == 1:
+        but_a_pressed=0
+
+player.finish()
