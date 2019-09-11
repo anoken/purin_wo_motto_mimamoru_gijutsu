e587d7e6207e192b295098c1e0b3a9212772c7f8
anoken <31365453+anoken@users.noreply.github.com>
2019-09-12 07:38:07 +0900

    Update 003_purin_learning.py

--- 06_pudding_alert_v_app/003_purin_learning.py
+++ 06_pudding_alert_v_app/003_purin_learning.py
@@ -2,15 +2,6 @@
 ## https://anoken.jimdo.com/
 ## https://github.com/anoken/purin_wo_motto_mimamoru_gijutsu
 
-import lcd  #for test
-from machine import I2C
-AXP192_ADDR=0x34
-Backlight_ADDR=0x91
-level=50
-i2c = I2C(I2C.I2C0, freq=100000, scl=28, sda=29)
-val = (level+7) << 4
-i2c.writeto_mem(AXP192_ADDR, Backlight_ADDR,int(val))
-
 import image, lcd, sensor
 import KPU as kpu
 from fpioa_manager import fm
