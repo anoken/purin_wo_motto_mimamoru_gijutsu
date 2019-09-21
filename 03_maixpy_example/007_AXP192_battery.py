8cc2ec72d68e7991463aab64895542161d338411
anoken <31365453+anoken@users.noreply.github.com>
2019-09-21 22:10:49 +0900

    Add files via upload

--- 03_maixpy_example/007_AXP192_battery.py
+++ 03_maixpy_example/007_AXP192_battery.py
@@ -1,4 +1,4 @@
-## Copyright (c) 2019 aNoken 
+## Copyright (c) 2019 aNoken
 ## https://anoken.jimdo.com/
 ## https://github.com/anoken/purin_wo_motto_mimamoru_gijutsu
 
@@ -20,13 +20,13 @@ while True:
     temp = axp.getTemperature()
 
     lcd.draw_string(20,0,"vbat:"+str(vbat))
-    lcd.draw_string(20,10,"usb_vol:"+str(usb_vol))
-    lcd.draw_string(20,20,"usb_cur:"+str(usb_cur))
-    lcd.draw_string(20,30,"connext_vol:"+str(connext_vol))
-    lcd.draw_string(20,40,"connext_input_current:"+str(connext_input_current))
-    lcd.draw_string(20,50,"bat_current:"+str(bat_current))
-    lcd.draw_string(20,60,"bat_dis_current:"+str(bat_dis_current))
-    lcd.draw_string(20,70,"bat_instant_watts:"+str(bat_instant_watts))
-    lcd.draw_string(20,80,"temp:"+str(temp))
+    lcd.draw_string(20,15,"usb_vol:"+str(usb_vol))
+    lcd.draw_string(20,30,"usb_cur:"+str(usb_cur))
+    lcd.draw_string(20,45,"connext_vol:"+str(connext_vol))
+    lcd.draw_string(20,60,"connext_input_current:"+str(connext_input_current))
+    lcd.draw_string(20,75,"bat_current:"+str(bat_current))
+    lcd.draw_string(20,90,"bat_dis_current:"+str(bat_dis_current))
+    lcd.draw_string(20,105,"bat_instant_watts:"+str(bat_instant_watts))
+    lcd.draw_string(20,120,"temp:"+str(temp))
 
 
