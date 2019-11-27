9468221d332513b9f4ae90569c6825c53276bad1
anoken <31365453+anoken@users.noreply.github.com>
2019-11-27 21:35:00 +0900

    Update nncase_convert.sh

--- 04_kmodel_learning/001_mnist/nncase_convert.sh
+++ 04_kmodel_learning/001_mnist/nncase_convert.sh
@@ -5,7 +5,7 @@ tflite_out=$name.tflite
 kmodel_out=$name.kmodel
 
 #nncase
-ncc_exe=./nncase/ncc
+ncc_exe=./ncc/ncc
 
 echo ">> Converting TFlite to Kmodel"
 $ncc_exe compile $tflite_out ./$kmodel_out -i tflite -o kmodel -t k210 --dataset dataset-all
