5e86be912e16a9a10fb8b36b481df98df25c8656
anoken <31365453+anoken@users.noreply.github.com>
2019-12-31 21:25:38 +0900

    Update 002_mbnet_keras_trainsfer_kpu.py

--- 04_kmodel_learning/002_mbnet_transfer/002_mbnet_keras_trainsfer_kpu.py
+++ 04_kmodel_learning/002_mbnet_transfer/002_mbnet_keras_trainsfer_kpu.py
@@ -2,16 +2,7 @@
 ## https://anoken.jimdo.com/
 ## https://github.com/anoken/purin_wo_motto_mimamoru_gijutsu
 
-#Maixpyの特定versionでmobilenetv1 1000-Class が読めない場合があります。
-#task = kpu.load("mbnet751.kmodel") or task = kpu.load(0x200000)
-# SYSCALL: Out of memory
-# ValueError: [MAIXPY]kpu: load error:6
-# が発生した場合、v0.4.0_47以前であれば動作します。
-#○:maixpy_v0.4.0_44_g95f00f0
-#○:maixpy_v0.4.0_47_g39bb8bf
-#×:maixpy_v0.4.0_49_g8279a1f
-#×:maixpy_v0.4.0_82_gc3327b5
-#https://github.com/sipeed/MaixPy/issues/180
+
 
 import sensor, image, lcd, time
 import KPU as kpu
@@ -29,7 +20,7 @@ lcd.draw_string(100,112,"Loading labels...")
 f=open('labels.txt','r')
 labels=f.readlines()
 f.close()
-task = kpu.load("mbnet751.kmodel")
+task = kpu.load("my_mbnet.kmodel")
 clock = time.clock()
 while(True):
     img = sensor.snapshot()
